a933697853970550d6a888377480b05af1a0da73
gabrielrochadesouza <108677907+gabrielrochadesouza@users.noreply.github.com>
2022-08-17 21:22:59 -0300

    Mandei#2

---
+++ src/main.js
@@ -0,0 +1,8 @@
+/* const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav.toggle')
+
+for (const element of taggle) {
+    element.addEventListenner('click', function ()) {
+        nav.classList.toggle('show)'
+    })
+}  */
